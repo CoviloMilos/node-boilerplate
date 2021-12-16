d02630092ab43b22d7e994ebc94433d8d38cafc5
milos.covilo@devoteam.com <milos.covilo@devoteam.com>
2021-12-16 19:25:16 +0100

    Http Client interceptor added

---
+++ server/interceptors/http-client-interceptor.ts
@@ -0,0 +1,38 @@
+import { injectable, unmanaged } from 'inversify';
+import axios, { AxiosInstance, AxiosResponse, AxiosRequestConfig } from 'axios';
+
+declare module 'axios' {
+  interface AxiosResponse<T = any> extends Promise<T> {}
+}
+
+@injectable()
+export abstract class HttpClient {
+  protected readonly instance: AxiosInstance;
+
+  public constructor(@unmanaged() baseURL: string) {
+    this.instance = axios.create({
+      baseURL,
+    });
+
+    this.initializeRequestInterceptor();
+    this.initializeResponseInterceptor();
+  }
+
+  private initializeRequestInterceptor = () => {
+    this.instance.interceptors.request.use((request: AxiosRequestConfig) => {
+      return request;
+    });
+  };
+
+  private initializeResponseInterceptor = () => {
+    this.instance.interceptors.response.use(this.handleResponse, this.handleError);
+  };
+
+  private handleResponse = ({ data }: AxiosResponse) => {
+    return data;
+  };
+
+  protected handleError = (error: any) => {
+    return Promise.reject(error);
+  };
+}
